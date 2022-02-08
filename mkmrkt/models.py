cea5cddc98dc1dad73a002fcf6fd700d4fafcd12
Josh <joshdanielrea@gmail.com>
2022-02-08 00:29:02 -0600

    fixed syntax issue and started serializers

---
+++ mkmrkt/models.py
@@ -0,0 +1,87 @@
+from django.conf import settings
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+ADDRESS_CHOICES = (
+    ('B', 'BILLING'),
+    ('S', 'SHIPPING'),
+)
+
+CATEGORY_CHOICES = {
+    ('LS', 'Lock-on shot'),
+    ('SF', 'Shoots forward'),
+    ('DB', 'Drops backward'),
+    ('FB', 'Shoots forward or backward'),
+    ('ML', 'Melee'),
+    ('ST', 'Status'),
+    ('OT', 'Other')
+}
+
+# MAY NOT NEED USERNAME AND PASSWORD HERE
+class User(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Address(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                            on_delete=models.CASCADE)
+    address_line1 = models.CharField(max_length=100)
+    address_line2 = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    postal_code = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    mobile = models.CharField(max_length=100)
+    address_type = models.CharField(max_length=1, choices=ADDRESS_CHOICES)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=225, db_index=True)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='images/')
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+class OrderItem(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                            on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)                        
+    quantity = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                            on_delete=models.CASCADE)
+    products = models.ManyToManyField(OrderItem)
+    first_name = models.CharField(max_length=60)
+	last_name = models.CharField(max_length=60)
+	email = models.EmailField()
+	address = models.CharField(max_length=150)
+	postal_code = models.CharField(max_length=30)
+	city = models.CharField(max_length=100)
+    start_date = models.DateTimeField(auto_now_add=True)
+    ordered_date = models.DateTimeField()
+    ordered = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
---
+++ mkmrkt/serializers.py
@@ -0,0 +1,97 @@
+from rest_framework import serializers
+from .models import User, Address, Product, OrderItem, Order
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    addresses = serializers.HyperlinkedRelatedField(
+        view_name='address_detail'
+    )
+
+    orders = serializers.HyperlinkedRelatedField(
+        view_name='order_detail'
+    )
+
+    user_url = serializers.ModelSerializer.serializer_url_field(
+        view_name ='user_detail'
+    )
+
+    class Meta:
+        model = User
+        fields = ('username')
+     #    determine auth model
+
+
+class AddressSerializer(serializers.HyperlinkedModelSerializer):
+    users = serializers.HyperlinkedRelatedField(
+        view_name ='user_detail'
+    )
+
+    orders = serializers.HyperlinkedRelatedField(
+        view_name ='order_detail'
+    )
+
+    address_url = serializers.ModelSerializer.serializer_url_field(
+        view_name ='address_detail'
+    )
+
+    class Meta:
+        model = Address
+        fields = ('users', 'address_line1', 'address_line2', 'city', 'orders'
+                  'postal_code', 'country', 'mobile', 'address_type', 'address_url')
+
+class ProductSerializer(serializers.HyperlinkedModelSerializer):             
+    order_item = serializers.HyperlinkedRelatedField(
+        view_name ='order_item_detail'
+    )
+
+    product_url = serializers.ModelSerializer.serializer_url_field(
+        view_name ='product_detail'
+    )
+
+    class Meta:
+        model = Product
+        fields = ('category', 'name', 'slug', 'description', 'image', 'price', 'available', 'order_item', 'product_url' )
+
+class OrderItemSerializer(serializers.HyperlinkedModelSerializer):
+
+    products = serializers.HyperlinkedRelatedField(
+        view_name='product_detail'
+    )
+
+    orders = serializers.HyperlinkedRelatedField(
+        view_name='order_detail'
+    )
+
+    order_item_url = serializers.ModelSerializer.serializer_url_field(
+        view_name='order_item_detail'
+    )
+
+    class Meta:
+        model = OrderItem
+        fields = ('product', 'price', 'quantity', 'products', 'orders', 'order_item_url')
+
+
+class OrderSerializer(serializers.HyperlinkedModelSerializer):
+
+    users = serializers.HyperlinkedRelatedField(
+        view_name ='user_detail'
+    )
+
+    orderitem = serializers.HyperlinkedRelatedField(
+        view_name='orderitem_detail'
+    )
+
+    addresses = serializers.HyperlinkedRelatedField(
+        view_name='address_detail'
+    )
+
+    order_url = serializers.ModelSerializer.serializer_url_field(
+        view_name='order_detail'
+    )
+
+    class Meta:
+        model = Order
+        fields = ('item', 'first_name', 'last_name', 'email', 'address', 'postal_code', 'city', 'start_date', 'ordered_date', 'ordered')
+
+
+        # left user out of meta fields for now
