0e725b54b252c61716f9dd181f1d5f02fc2afad0
Josh <joshdanielrea@gmail.com>
2022-02-10 13:42:16 -0600

    installed dependencies, styled home and cards

--- mario-kart-mrkt/src/store/index.js
+++ mario-kart-mrkt/src/store/index.js
@@ -1,15 +1,14 @@
-// import { createStore, combineReducers, applyMiddleware } from 'redux'
-// import { composeWithDevTools } from 'redux-devtools-extension'
-// import PostReducer from './reducers/PostReducer'
-// import CommentReducer from './reducers/CommentReducer'
-// import thunk from 'redux-thunk'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import ProductReducer from './reducers/ProductReducer'
+import thunk from 'redux-thunk'
 
-// const store = createStore(
-//     combineReducers({
-//       postState: PostReducer,
-//       commentState: CommentReducer
-//     }),
-//     composeWithDevTools(applyMiddleware(thunk)))
+const store = createStore(
+    combineReducers({
+      productState: ProductReducer
+    //   commentState: CommentReducer
+    }),
+    composeWithDevTools(applyMiddleware(thunk)))
   
   
-//   export default store
+  export default store
