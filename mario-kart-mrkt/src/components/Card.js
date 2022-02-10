c258dd81a9e1c12afce1ed629579e263866cb8e0
Josh <joshdanielrea@gmail.com>
2022-02-10 01:34:26 -0600

    redux work

--- mario-kart-mrkt/src/components/Card.js
+++ mario-kart-mrkt/src/components/Card.js
@@ -2,13 +2,15 @@ import React from 'react'
 
 export default function Card(props) {
     let newClassName = 'color_bg ${props.alt}'
-    let bg_img = 'url(${props.images})'
+    let bg_img = '({props.images})'
     let { name, description, price, available } = props
     return (
         <div className="card">
             <div className="wrapper">
                 <div className={newClassName}>
-                    <div className="card_img" style={{ "backgroundImage": bg_img }}></div>
+                    <div className="card_img" style={{ "backgroundImage": bg_img }}>
+                        
+                    </div>
                     <div className="heart">
                         {/* svg here */}
                     </div>
--- mario-kart-mrkt/src/pages/Home.js
+++ mario-kart-mrkt/src/pages/Home.js
@@ -1,45 +1,47 @@
 import React from "react";
 import "../index.css"
 import Card from "../components/Card";
+import shellImg from '../assets/spinyshell.jpeg'
 
 function Home() {
-  return (
-    <div className="container">
-      <div className="row">
-        <Card
-          name="Spiny Shell"
-          description="Although the Spiny Shell has taken on many different forms and variations throughout the series, its main objective has always remained the same: to utterly obliterate the front runner in the race."
-          images="../assets/spinyshell.jpeg"
-          price="5,999"
-          available={true}
-        />
-        
-        <Card
-          name="Spiny Shell"
-          description="Although the Spiny Shell has taken on many different forms and variations throughout the series, its main objective has always remained the same: to utterly obliterate the front runner in the race."
-          images="../assets/spinyshell.jpeg"
-          price="5,999"
-          available={true}
-        />
+    return (
 
-<Card
-          name="Spiny Shell"
-          description="Although the Spiny Shell has taken on many different forms and variations throughout the series, its main objective has always remained the same: to utterly obliterate the front runner in the race."
-          images="../assets/spinyshell.jpeg"
-          price="5,999"
-          available={true}
-        />
+        <div className="container">
+            <div className="row">
+                <Card
+                    name="Spiny Shell"
+                    description="Although the Spiny Shell has taken on many different forms and variations throughout the series, its main objective has always remained the same: to utterly obliterate the front runner in the race."
+                    images={shellImg}
+                    price="5,999"
+                    available={true}
+                />
 
-<Card
-          name="Spiny Shell"
-          description="Although the Spiny Shell has taken on many different forms and variations throughout the series, its main objective has always remained the same: to utterly obliterate the front runner in the race."
-          images="../assets/spinyshell.jpeg"
-          price="5,999"
-          available={true}
-        />
-      </div>
-    </div>
-  );
+                <Card
+                    name="Spiny Shell"
+                    description="Although the Spiny Shell has taken on many different forms and variations throughout the series, its main objective has always remained the same: to utterly obliterate the front runner in the race."
+                    images={shellImg}
+                    price="5,999"
+                    available={true}
+                />
+
+                <Card
+                    name="Spiny Shell"
+                    description="Although the Spiny Shell has taken on many different forms and variations throughout the series, its main objective has always remained the same: to utterly obliterate the front runner in the race."
+                    images={shellImg}
+                    price="5,999"
+                    available={true}
+                />
+
+                <Card
+                    name="Spiny Shell"
+                    description="Although the Spiny Shell has taken on many different forms and variations throughout the series, its main objective has always remained the same: to utterly obliterate the front runner in the race."
+                    images={shellImg}
+                    price="5,999"
+                    available={true}
+                />
+            </div>
+        </div>
+    );
 }
 
 export default Home;
