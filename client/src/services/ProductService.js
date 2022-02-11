32e2f65c2e43dd493a450d089a1114931bf3d044
Josh <joshdanielrea@gmail.com>
2022-02-11 09:33:08 -0600

    changed react parent folder name to client and added actions to redux

---
+++ client/src/services/ProductService.js
@@ -0,0 +1,46 @@
+import Client from '.'
+
+export const GetProducts = async () => {
+    try {
+        const res = await Client.get('/products')
+        return res.data.products
+    } catch (error) {
+        throw error
+    }
+}
+
+export const GetProductsById = async (id) => {
+    try {
+        const res = await Client.get(`/products/${id}`)
+        return res.data.products
+    } catch (error) {
+        throw error
+    }
+}
+
+export const NewProducts = async (newProduct) => {
+    try {
+        const res = await Client.product(`/products`, newProduct)
+        return res.data.products
+    } catch (error) {
+        throw error
+    }
+}
+
+export const UpdateProduct = async (updatedProduct) => {
+    try {
+        const res = await Client.put(`/products/${updatedProduct._id}`, updatedProduct)
+        return res.data
+    } catch (error) {
+        throw error
+    }
+}
+
+export const DeleteProduct = async (id) => {
+    try {
+        const res = await Client.delete(`/products/${id}`)
+        console.log('delete res', res) 
+    } catch (error) {
+        throw error
+    }
+}
