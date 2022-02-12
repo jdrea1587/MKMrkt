e871de9daa4c0fdc94089b74e19b15775c99f674
Josh <joshdanielrea@gmail.com>
2022-02-12 15:12:28 -0600

    homepage title style add

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,6 +5,18 @@ import { Link } from 'react-router-dom';
 import '../index.css';
 import Card from '../components/Card';
 import { LoadProducts } from '../store/actions/ProductAction';
+import styled from 'styled-components';
+
+const StyledTitle = styled.h1`
+    width: 100%;
+    height: 100%;
+    padding: 30px;
+	margin-top: 20px;
+    text-align: center;
+    margin: auto;
+    @media (max-width: 500px) {
+        font-size: 1rem;
+`;
 
 const mapStateToProps = ({ productState }) => {
 	return { productState };
@@ -21,22 +33,25 @@ function Home(props) {
 		props.fetchProducts();
 	}, []);
 	return (
-		<div className='container'>
-			<div className='row'>
-				{props.productState.products.map((product) => (
-					<Link to={`/productview/${product.id}`} key={product.id} product={product}>
-						<Card
-							key={product.slug}
-							name={product.name}
-							description={product.description}
-							price={product.price}
-							image={product.image}
-							available={product.available}
-						/>
-					</Link>
-				))}
+		<>
+			<StyledTitle>MarioKart Blk Mrkt</StyledTitle>
+			<div className='container'>
+				<div className='row'>
+					{props.productState.products.map((product) => (
+						<Link to={`/productview/${product.id}`} key={product.id} product={product}>
+							<Card
+								key={product.slug}
+								name={product.name}
+								description={product.description}
+								price={product.price}
+								image={product.image}
+								available={product.available}
+							/>
+						</Link>
+					))}
+				</div>
 			</div>
-		</div>
+		</>
 	);
 }
 
