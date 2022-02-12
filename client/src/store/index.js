29cede5b6d58a7d8c7e009228159a85cc55d38c2
Josh <joshdanielrea@gmail.com>
2022-02-12 14:58:37 -0600

    indentation and formatting, and cart psuedo

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,14 +1,14 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension'
-import ProductReducer from './reducers/ProductReducer'
-import thunk from 'redux-thunk'
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import ProductReducer from './reducers/ProductReducer';
+import thunk from 'redux-thunk';
 
 const store = createStore(
-    combineReducers({
-      productState: ProductReducer
-    //   commentState: CommentReducer
-    }),
-    composeWithDevTools(applyMiddleware(thunk)))
-  
-  
-  export default store
+	combineReducers({
+		productState: ProductReducer,
+		//   commentState: CommentReducer
+	}),
+	composeWithDevTools(applyMiddleware(thunk))
+);
+
+export default store;
