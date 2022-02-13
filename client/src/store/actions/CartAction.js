3eccfa43c5e213ff8e3a36c28b69f0011af9e219
Josh <joshdanielrea@gmail.com>
2022-02-12 20:00:51 -0600

    cleaned up redux and worked on cart shop

--- client/src/store/actions/CartAction.js
+++ client/src/store/actions/CartAction.js
@@ -1,32 +1,51 @@
 // import {
 //     GetProducts, GetProductBySlug, NewProduct, UpdateProduct, DeleteProduct
 // } from '../../services/CartService'
-import { ADD_TO_CART, REMOVE_FROM_CART, UPDATE_QUANTITY, SUB_QUANTITY, EMPTY_CART } from '../types';
+import {
+  ADD_TO_CART,
+  REMOVE_FROM_CART,
+  UPDATE_QUANTITY,
+  EMPTY_CART,
+  ADD_USER_INFO,
+  PLACE_ORDER,
+} from '../types';
 
-export const addToCart = (orderId, productId) => {
-	return {
-		type: ADD_TO_CART,
-		orderId,
-		productId,
-	};
+//orderItem = {OrderItem obj}
+export const addToCart = (orderItem) => {
+  return {
+    type: ADD_TO_CART,
+    orderItem,
+  };
 };
 export const removeFromCart = (orderItemId) => {
-	return {
-		type: REMOVE_FROM_CART,
-		orderItemId,
-	};
+  return {
+    type: REMOVE_FROM_CART,
+    orderItemId,
+  };
 };
 export const updateQuantity = (orderItemId, quantity) => {
-	return {
-		type: UPDATE_QUANTITY,
-		orderItemId,
-		quantity,
-	};
+  return {
+    type: UPDATE_QUANTITY,
+    orderItemId,
+    quantity,
+  };
 };
 
 export const emptyCart = (orderId) => {
-	return {
-		type: EMPTY_CART,
-		orderId,
-	};
+  return {
+    type: EMPTY_CART,
+    orderId,
+  };
+};
+
+export const addUserInfo = () => {
+  return {
+    type: ADD_USER_INFO,
+  };
+};
+
+export const placeOrder = () => {
+  return {
+    type: PLACE_ORDER,
+  };
 };
