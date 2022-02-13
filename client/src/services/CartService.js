2d29b26c865111fd334452d6f71a5aed42692097
Josh <joshdanielrea@gmail.com>
2022-02-12 20:31:43 -0600

    more reducer refactoring

--- client/src/services/CartService.js
+++ client/src/services/CartService.js
@@ -1,1 +1,56 @@
 import Client from '.';
+
+export const GetCartById = async (id) => {
+  try {
+    const res = await Client.get(`/orders/${id}`);
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const CreateNewCart = async () => {
+  try {
+    const res = await Client.post('/orders/');
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const AddOrderItem = async (newItem) => {
+  try {
+    //newItem {orderId: ##, productID: ##}
+    const res = await Client.post('/orderitem/', newItem);
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const UpdateOrderItem = async (id) => {
+  try {
+    const res = await Client.put(`/orderitem/${id}`);
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const DeleteOrderItem = async (id) => {
+  try {
+    const res = await Client.delete(`/orderitem/${id}`);
+    return res.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const EmptyCart = async (id) => {
+  try {
+    const res = await Client.delete(`/orders/${id}`);
+    console.log('delete res', res);
+  } catch (error) {
+    throw error;
+  }
+};
