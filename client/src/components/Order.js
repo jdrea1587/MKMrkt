9ddc885faa7048434ee9cf236018c0d3dfd04f55
Josh <joshdanielrea@gmail.com>
2022-02-14 02:47:01 -0600

    cleaned syntax

--- client/src/components/Order.js
+++ client/src/components/Order.js
@@ -2,23 +2,11 @@ import React from 'react';
 import '../index.css';
 import { Link } from 'react-router-dom';
 
-function Order({ cart }) {
-  console.log('cart in order', cart);
-  const calculateQuantity = () => {
-    let quantity = 0;
-    cart.products.forEach((product) => quantity + product.quantity);
-    return quantity;
-  };
-  console.log(calculateQuantity());
-
-  //cart.
-  //create function that counts number of products in order
-  //create function that adds prices.
-
+function Order() {
   return (
     <div className='cartstatus'>
       <div>
-        <h1>Savvy</h1>
+        <h1>Savvy?</h1>
         <Link to={'/checkout/'}>
           <button>Checkout</button>
         </Link>
